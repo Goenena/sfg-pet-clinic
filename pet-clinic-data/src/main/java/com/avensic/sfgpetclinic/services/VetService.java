8be7b2aea0edb62d94a7b029c56c3b3b96820fdc
Aaron <aaron.goenen@gmail.com>
2022-02-10 12:18:30 +0100

    Add Interfaces. Closes #5

---
+++ pet-clinic-data/src/main/java/com/avensic/sfgpetclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package com.avensic.sfgpetclinic.services;
+
+import com.avensic.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
