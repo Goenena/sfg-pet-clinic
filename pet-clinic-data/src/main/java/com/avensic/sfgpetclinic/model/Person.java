1aafb5d6425cf3bcc439e7df76e8df500d0ec91a
Aaron <aaron.goenen@gmail.com>
2022-02-10 12:24:22 +0100

    Adding Base Entity. Closes #6

--- pet-clinic-data/src/main/java/com/avensic/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/avensic/sfgpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.avensic.sfgpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
 
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/com/avensic/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/avensic/sfgpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package com.avensic.sfgpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
 
     private String name;
 
