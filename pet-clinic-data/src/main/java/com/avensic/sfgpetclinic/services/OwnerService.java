fd79a1ce1d753f9a6523cec694293639638d491d
Aaron <aaron.goenen@gmail.com>
2022-02-24 09:54:11 +0100

    Refactor Service Interfaces. Closes #11

--- pet-clinic-data/src/main/java/com/avensic/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/avensic/sfgpetclinic/services/OwnerService.java
@@ -2,15 +2,8 @@ package com.avensic.sfgpetclinic.services;
 
 import com.avensic.sfgpetclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long>{
 
     Owner findByLastName(String lastName);
 
-    Owner findById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
 }
--- pet-clinic-data/src/main/java/com/avensic/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/avensic/sfgpetclinic/services/PetService.java
@@ -2,13 +2,6 @@ package com.avensic.sfgpetclinic.services;
 
 import com.avensic.sfgpetclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
