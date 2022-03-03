efe85bbe57695b0f0662cfb4998997fa8d236009
Aaron <aaron.goenen@gmail.com>
2022-03-03 11:41:34 +0100

    Auto Generate Map IDs. Closes #23

--- pet-clinic-web/src/main/java/com/avensic/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/avensic/sfgpetclinic/bootstrap/DataLoader.java
@@ -22,14 +22,12 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Aaron");
         owner1.setLastName("Goenen");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Kyra");
         owner2.setLastName("de Vries");
 
@@ -38,14 +36,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners... ");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Serious");
         vet1.setLastName("Doctor");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Funny");
         vet2.setLastName("Medic");
 
