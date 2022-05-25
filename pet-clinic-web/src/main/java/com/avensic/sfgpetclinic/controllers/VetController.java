85e760d27529ead6f3d208092b7f7b8bc4eb746b
Aaron <aaron.goenen@gmail.com>
2022-05-25 10:48:53 +0200

    Fix Menus. closes #46

--- pet-clinic-web/src/main/java/com/avensic/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/avensic/sfgpetclinic/controllers/VetController.java
@@ -14,7 +14,7 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"/vets", "/vets/index.html", "/vets/index"})
+    @RequestMapping({"/vets", "/vets/index.html", "/vets/index", "/vets.html"})
     public String listVets(Model model){
 
         model.addAttribute("vets", vetService.findAll());
