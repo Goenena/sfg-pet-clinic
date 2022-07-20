6fb311ed811b9e4996e9850446485249f9794d49
Aaron <aaron.goenen@gmail.com>
2022-07-20 17:42:13 +0200

    Add JPA implementations. Closes #39, #41, #42, #43, #44

--- pet-clinic-data/src/main/java/com/avensic/sfgpetclinic/services/springdatajpa/OwnerSDJpaService.java
+++ pet-clinic-data/src/main/java/com/avensic/sfgpetclinic/services/springdatajpa/OwnerSDJpaService.java
@@ -28,11 +28,8 @@ public class OwnerSDJpaService  implements OwnerService {
 
     @Override
     public Set<Owner> findAll() {
-
         Set<Owner> owners = new HashSet<>();
-
         ownerRepository.findAll().forEach(owners::add);
-
         return owners;
     }
 
